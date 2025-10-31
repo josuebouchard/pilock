390f3b98c80f27fc3c05026cb342e922cfd81ef3
Josué Bouchard <7890663+josuebouchard@users.noreply.github.com>
2025-10-31 16:39:30 -0400

    focus the scope of the project

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request, APIRouter
+from templates import templates
 
 # === START CREATE DB TABLES ===
 from db import create_db_and_tables
-
-from .ui.tags import router as tag_router
-
-# Routers
-from .ui.users import router as user_mgmt_router
 
 create_db_and_tables()
 # === END CREATE DB TABLES ===
@@ -16,11 +11,45 @@ create_db_and_tables()
 # Create FastAPI main controller
 app = FastAPI()
 
-# Add routers
-app.include_router(user_mgmt_router)
+# Tags
+tag_router = APIRouter(prefix="/tags")
+
+
+@tag_router.get("/", name="tags:index-ui")
+def tag_index(request: Request):
+    return templates.TemplateResponse(
+        request,
+        name="tags/index.jinja",
+        context={},
+    )
+
+
+@tag_router.get("/create", name="tags:create-ui")
+def tag_create(request: Request):
+    return templates.TemplateResponse(
+        request,
+        name="tags/create.jinja",
+        context={
+            "form": {},  # optional dict to prefill (empty for new)
+            "errors": {},  # optional dict for validation errors
+            "csrf_token": None,
+        },
+    )
+
+
+# Access log
+logs_router = APIRouter(prefix="/logs")
+
+
+@logs_router.get("/", name="logs:index-ui")
+def access_log_index(request: Request):
+    return templates.TemplateResponse(
+        request,
+        name="logs/index.jinja",
+        context={},
+    )
+
+
+# Set routers
 app.include_router(tag_router)
-
-
-@app.get("/")
-def home(request: Request):
-    return RedirectResponse(request.url_for("users:index"))
+app.include_router(logs_router)
